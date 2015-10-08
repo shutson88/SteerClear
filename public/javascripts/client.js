6308a6273480f4967de290fd41d871a2df488bdb
monkeyulize <sholberg92@gmail.com>
2015-10-08 16:08:23 -0500

    Changed register process, added redirect after success
    Previously registering was a post request within the html page.  By
    using a function to call the post request the server can send json which
    can be used to update the existing page with success, and initiate a
    redirect.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,6 @@
 var token = "";
 
-function authenticate() {
+function login() {
   
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -13,8 +13,8 @@ function authenticate() {
     	}
     	else{
     		token = obj.token;
-        window.localStorage.setItem('token', token);
-        window.localStorage.setItem('username', document.getElementById("username").value);
+			window.localStorage.setItem('token', token);
+			window.localStorage.setItem('username', document.getElementById("username").value);
     		window.location.assign("http://" + window.location.host + "/index");
     	}
 		
@@ -26,6 +26,38 @@ function authenticate() {
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhttp.send("username="+document.getElementById("username").value+"&password="+document.getElementById("password").value);
 }
+
+function register() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			var obj = JSON.parse(xhttp.responseText);
+			
+			if(obj.success != true) {
+				alert(obj.message);
+			}
+			else {
+				
+				document.getElementById("successDiv").textContent = "Successfully Registered...redirecting in 2 seconds...";
+				setTimeout(function() {
+					window.location.replace("http://" + window.location.host + "/index");
+				}, 2000);
+				
+				
+				
+			}
+		}
+		
+	}
+	xhttp.open("POST", "http://" + window.location.host + "/register", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(
+		"username="+document.getElementById("username").value + 
+		"&password="+document.getElementById("password").value); 
+		//TODO: modify when the user model is changed to match
+	
+}
+
 
 function loadHomepage(){
 	var xhttp = new XMLHttpRequest();
