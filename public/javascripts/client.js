adecf03182f59d00ec338b8b8b9b356d1955337d
Jeremy Moorman <jsmoorman@tamu.edu>
2015-10-10 11:23:13 -0500

    Finished user schema, but unique validation is not working

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,7 +1,7 @@
 var token = "";
 
 function login() {
-  
+
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -17,7 +17,7 @@ function login() {
 			window.localStorage.setItem('username', document.getElementById("username").value);
     		window.location.assign("http://" + window.location.host + "/index");
     	}
-		
+
       	//console.log(xhttp.responseText);
       	//console.log("token: " + token);
     }
@@ -32,30 +32,28 @@ function register() {
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			var obj = JSON.parse(xhttp.responseText);
-			
+
 			if(obj.success != true) {
 				alert(obj.message);
 			}
 			else {
-				
+
 				document.getElementById("successDiv").textContent = "Successfully Registered...redirecting in 2 seconds...";
 				setTimeout(function() {
 					window.location.replace("http://" + window.location.host + "/index");
 				}, 2000);
-				
-				
-				
 			}
 		}
-		
 	}
 	xhttp.open("POST", "http://" + window.location.host + "/register", true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send(
-		"username="+document.getElementById("username").value + 
-		"&password="+document.getElementById("password").value); 
+		"username="+document.getElementById("username").value +
+		"&password="+document.getElementById("password").value +
+        "&first_name="+document.getElementById("first_name").value +
+        "&last_name="+document.getElementById("last_name").value +
+        "&email="+document.getElementById("email").value);
 		//TODO: modify when the user model is changed
-	
 }
 
 function addanimal() {
@@ -63,45 +61,45 @@ function addanimal() {
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			var obj = JSON.parse(xhttp.responseText);
-			
+
 			if(obj.success != true) {
 				alert(obj.message);
 			}
 			else {
-				
+
 				document.getElementById("successDiv").textContent = "Added successfully...redirecting in 2 seconds...";
 				setTimeout(function() {
 					window.location.replace("http://" + window.location.host + "/index");
 				}, 2000);
-				
-				
-				
+
+
+
 			}
 		}
-		
-	}	
+
+	}
 	xhttp.open("POST", "http://" + window.location.host + "/api/addanimal", true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send(
-		"id="+document.getElementById("id").value + 
+		"id="+document.getElementById("id").value +
 		"&managedBy="+document.getElementById("managedBy").value +
 		"&name="+document.getElementById("name").value +
 		"&type="+document.getElementById("type").value +
 		"&breed="+document.getElementById("breed").value +
 		"&date="+document.getElementById("date").value +
 		"&latestWeight="+document.getElementById("latestWeight").value +
-		"&token="+window.localStorage.getItem('token')); 
-		//TODO: modify when the animal model is changed	
-	
-	
-	
+		"&token="+window.localStorage.getItem('token'));
+		//TODO: modify when the animal model is changed
+
+
+
 }
 
 function loadHomepage(){
 	var xhttp = new XMLHttpRequest();
   xhttp.open("GET", "http://" + window.location.host + "/index", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.onreadystatechange = function() { 
+  xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200){
      // console.log(xhttp.responseText);
       window.location.assign("http://" + window.location.host + "/index")
