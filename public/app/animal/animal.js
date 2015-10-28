c503575a25035f89f24a3f985901bb7e83d4b4ef
Seth <shutson@tamu.edu>
2015-10-28 12:08:05 -0500

    Merge branch 'angular-integration' of https://github.com/shutson88/SteerClear into angular-integration
    # Conflicts:
    #	public/app/animal/animal.js
    Fix merge conflicts

--- public/app/animal/animal.js
+++ public/app/animal/animal.js
@@ -53,119 +53,114 @@ angular.module('app.animal', ['ngRoute', 'animalService', 'ui.bootstrap', 'filte
         //  $scope.status = status;
         //});
       }
-		
 
-    vm.getAverageArray = function(){
-      var regressionData = new Array();
-		
-	
-	
-	  vm.calculate = function($scope) {
-		  var regressionData = new Array();
 
-      for(var i = 0; i < vm.animals.length; i++) {
+    vm.getAverageArray = function() {
 
-        regressionData.push([vm.animals[i].date, vm.animals[i].weight]); //build array [date, weight] for regression fitting
-      }
+        var regressionData = new Array();
 
-      regressionData.sort(function(a,b) {
-        return new Date(a[0]) - new Date(b[0]); //sort array from earliest to latest
-      });
+        for (var i = 0; i < vm.animals.length; i++) {
 
-      //Calculate average of each day to make unique date array
-      var averageDates = new Array();
-      var currentDate = regressionData[0][0];
-      var averageWeight = 0;
-      var averageSum = 0;
-
-      console.log("Current date: " + currentDate);
-      console.log(regressionData.length);
-      for (var i = 0; i < regressionData.length; i++){
-        if(currentDate === regressionData[i][0]){
-          averageWeight += regressionData[i][1];
-          averageSum++;
+          regressionData.push([vm.animals[i].date, vm.animals[i].weight]); //build array [date, weight] for regression fitting
         }
-        else{
-          averageDates.push([currentDate, averageWeight/averageSum]);
-          currentDate = regressionData[i][0];
-          averageWeight = regressionData[i][1];
-          averageSum = 1;
-        }
-        if(i === regressionData.length - 1){
 
-          averageDates.push([currentDate, averageWeight/averageSum]);
-          currentDate = regressionData[i][0];
-          averageWeight = regressionData[i][1];
-        }
-      }
-      console.log("getAvgArray:" + averageDates);
-      return averageDates;
+        regressionData.sort(function (a, b) {
+          return new Date(a[0]) - new Date(b[0]); //sort array from earliest to latest
+        });
 
+        //Calculate average of each day to make unique date array
+        var averageDates = new Array();
+        var currentDate = regressionData[0][0];
+        var averageWeight = 0;
+        var averageSum = 0;
 
-    }
+        console.log("Current date: " + currentDate);
+        console.log(regressionData.length);
+        for (var i = 0; i < regressionData.length; i++) {
+          if (currentDate === regressionData[i][0]) {
+            averageWeight += regressionData[i][1];
+            averageSum++;
+          }
+          else {
+            averageDates.push([currentDate, averageWeight / averageSum]);
+            currentDate = regressionData[i][0];
+            averageWeight = regressionData[i][1];
+            averageSum = 1;
+          }
+          if (i === regressionData.length - 1) {
 
-	  vm.calculate = function($scope) {
-
-      //console.log("Average dates"+averageDates);
-      var regressionData = vm.getAverageArray();
-
-      //console.log("Regression data:" + regressionData);
-		  var referenceDate = regressionData[0][0]; //take the earliest date as a referece
-		  
-		  for(var i = 0; i < regressionData.length; i++) {
-			  var second = new Date(regressionData[i][0]);
-			  var first = new Date(referenceDate);
-			regressionData[i][0] = Math.round((second-first)/(1000*60*60*24));
-			  console.log("Days since start : " + Math.round((second-first)/(1000*60*60*24)));		
-			
-		  }
-		  //TODO: ensure that each date is unique so that the data can be used to form a regression line
-		  console.log(regressionData);
-		  
-		  if(vm.targetWeight) {
-			  console.log("target weight");
-			  
-		  } else if(vm.targetDate) {
-			  console.log("target date");
-			  
-		  }
-
-	  }
-
-    vm.averageGainInRange = function(){
-      if(!vm.start_date || !vm.end_date){
-        vm.data = "Please enter start date and end date."
-      }
-      else if(vm.start_date > vm.end_date){
-        vm.data = "Start date must be before end date."
-      }
-      else{
-        var averageDates = vm.getAverageArray();
-
-        //console.log("averageDates: " + averageDates);
-
-        var dailyGains = new Array();
-        for(var i = 0; i < averageDates.length; i++){
-          console.log(averageDates[i][1]);
-
-          if(i < averageDates.length-1 && new Date(averageDates[i][0]) >= new Date(vm.start_date) && new Date(vm.end_date) >= new Date(averageDates[i+1][0])) {
-            dailyGains.push(averageDates[i+1][1] - averageDates[i][1])
+            averageDates.push([currentDate, averageWeight / averageSum]);
+            currentDate = regressionData[i][0];
+            averageWeight = regressionData[i][1];
           }
         }
-        var averageDailyGain = 0;
-        //console.log("Daily gains: " + dailyGains);
-        for(var i = 0; i < dailyGains.length; i++){
-          averageDailyGain += dailyGains[i];
+        console.log("getAvgArray:" + averageDates);
+        return averageDates;
+
+
+      }
+
+      vm.calculate = function ($scope) {
+
+        //console.log("Average dates"+averageDates);
+        var regressionData = vm.getAverageArray();
+
+        //console.log("Regression data:" + regressionData);
+        var referenceDate = regressionData[0][0]; //take the earliest date as a referece
+
+        for (var i = 0; i < regressionData.length; i++) {
+          var second = new Date(regressionData[i][0]);
+          var first = new Date(referenceDate);
+          regressionData[i][0] = Math.round((second - first) / (1000 * 60 * 60 * 24));
+          console.log("Days since start : " + Math.round((second - first) / (1000 * 60 * 60 * 24)));
+
         }
-        if(dailyGains.length === 0){
-          vm.data = "Average daily weight gain: 0";
+        //TODO: ensure that each date is unique so that the data can be used to form a regression line
+        console.log(regressionData);
+
+        if (vm.targetWeight) {
+          console.log("target weight");
+
+        } else if (vm.targetDate) {
+          console.log("target date");
+
+        }
+
+      }
+
+      vm.averageGainInRange = function () {
+        if (!vm.start_date || !vm.end_date) {
+          vm.data = "Please enter start date and end date."
+        }
+        else if (vm.start_date > vm.end_date) {
+          vm.data = "Start date must be before end date."
         }
         else {
-          vm.data = "Average daily weight gain: " + averageDailyGain / dailyGains.length;
+          var averageDates = vm.getAverageArray();
+
+          //console.log("averageDates: " + averageDates);
+
+          var dailyGains = new Array();
+          for (var i = 0; i < averageDates.length; i++) {
+            console.log(averageDates[i][1]);
+
+            if (i < averageDates.length - 1 && new Date(averageDates[i][0]) >= new Date(vm.start_date) && new Date(vm.end_date) >= new Date(averageDates[i + 1][0])) {
+              dailyGains.push(averageDates[i + 1][1] - averageDates[i][1])
+            }
+          }
+          var averageDailyGain = 0;
+          //console.log("Daily gains: " + dailyGains);
+          for (var i = 0; i < dailyGains.length; i++) {
+            averageDailyGain += dailyGains[i];
+          }
+          if (dailyGains.length === 0) {
+            vm.data = "Average daily weight gain: 0";
+          }
+          else {
+            vm.data = "Average daily weight gain: " + averageDailyGain / dailyGains.length;
+          }
         }
       }
-    }
-
 
     }]);
 
